841dbe07a566ab021d05648884039eb05a073016
John Palaganas <112512965+Johnpepsi@users.noreply.github.com>
2023-12-30 22:25:34 -0600

    Add files via upload

---
+++ react-props-practice-forked/src/contacts.js
@@ -0,0 +1,25 @@
+const contacts = [
+  {
+    name: "John Palaganas",
+    imgURL:
+      "https://avatars.githubusercontent.com/u/112512965?s=400&u=24e91e552a51316237a4a181c3abd33732c44ed0&v=4",
+    phone: "306-316-9211",
+    email: "kobebryantz143@gmail.com",
+  },
+  {
+    name: "Budoy Anderson",
+    imgURL:
+      "https://i0.wp.com/starmometer.com/wp-content/uploads/2015/12/Gerald-Anderson.jpg?fit=951%2C535&ssl=1",
+    phone: "+987 654 321",
+    email: "budoyislost@nowhere.com",
+  },
+  {
+    name: "Lee me ho",
+    imgURL:
+      "https://imageio.forbes.com/specials-images/imageserve/64a551ede0915b806ab4ddd4/T1-League-of-Legends-player-Faker-poses-for-the-camera/960x0.jpg?format=jpg&width=960",
+    phone: "+918 372 574",
+    email: "leebagho.com",
+  },
+];
+
+export default contacts;
